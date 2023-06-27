9d99df97380372d5761fdd110f2b4bbdc3582177
Edy Galdiolo <edygaldy302@gmail.com>
2023-06-27 18:14:29 +0200

    Feat(header): added header

--- config/store.php
+++ config/store.php
@@ -16,4 +16,46 @@ return [
         ]
     ],
     'someOtherData' => [ 42, 37, 73],
+    'navLinks' => [
+        [
+            "href" => "/otherPage",
+            "text" => "Characters",
+        ],
+        [
+            "href" => "/otherpage",
+            "text" => "Comics",
+        ],
+        [
+            "href" => "/otherPage",
+            "text" => "Movies",
+        ],
+        [
+            "href" => "/otherPage",
+            "text" => "TV",
+        ],
+        [
+            "href" => "/otherPage",
+            "text" => "Games",
+        ],
+        [
+            "href" => "/otherPage",
+            "text" => "Collectibles",
+        ],
+        [
+            "href" => "/otherPage",
+            "text" => "Videos",
+        ],
+        [
+            "href" => "/otherPage",
+            "text" => "Fans",
+        ],
+        [
+            "href" => "/otherPage",
+            "text" => "News",
+        ],
+        [
+            "href" => "/otherPage",
+            "text" => "Shop",
+        ]
+    ],
 ];
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,27 @@
-<header class="p-3">
-    <img class="logo" src="{{ Vite::asset('resources/img/logo.png') }}" alt="Laravel Logo">
-    <span>Some links: </span>
-    @foreach ($links as $link)
-        <a href="{{ $link['href'] }}">{{ $link['text'] }}</a>
-    @endforeach
+<header>
+    <div class="bg-primary">
+        <div class="container">
+            <div class="row d-flex justify-content-end">
+                <div class="col-2 d-flex justify-content-between">
+                    <a href="#" class="text-light"><small>Lorem</small></a>
+                    <a href="#" class="text-light"><small>Lorem</small></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container p-3">
+        <div class="row">
+            <div class="col d-flex justify-content-center align-items-center">
+                <img class="logo" src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="Laravel Logo">
+            </div>
+            <div class="col-8 d-flex justify-content-between align-items-center">
+            @foreach ($links as $link)
+                <a href="{{ $link['href'] }}">{{ $link['text'] }}</a>
+            @endforeach
+            </div>
+            <div class="col d-flex justify-content-end align-items-center">
+                <p class="m-0">Search</p>
+            </div>
+        </div>
+    </div>
 </header>
